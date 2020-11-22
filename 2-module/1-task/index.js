5d0814fdb2db077d8526dac175f52a52bd4a5ae2
aleksey <alekseyt@shtrafy-gibdd.ru>
2020-11-22 16:31:07 +0300

    2-module/1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,31 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
+console.log(sumSalary(salaries));
+
 function sumSalary(salaries) {
-  // ваш код...
+	let sum = 0;
+  	
+  	for(let i in salaries) {
+  		let val = salaries[i];
+  		if(isNumeric(val)) {
+  			sum += val;
+  		}
+  	}
+
+  	return sum;
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
